117fe7768f85d34be053343ce863f92b4b54ea6a
dlgustjf147 <kn4711@naver.com>
2020-11-01 17:09:18 +0900

    Initial commit

---
+++ app/src/main/java/com/example/project5/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.project5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                databaseReference.child("message").push().setValue("2");
+
+            }
+        });
+
+    }
+
+}
